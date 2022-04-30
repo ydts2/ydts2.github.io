c9d05c7579eff80436fc4701d49de3f2b49bf99e
ydts2 <ykpetrolhead08@outlook.com>
2022-04-30 17:51:44 +0530

    added functionality to the website

--- web/script.js
+++ web/script.js
@@ -1,28 +1,61 @@
-const RANDOM_QUOTE_URL = 'https://api.quotable.io/random'
-const displayquote = document.getElementById('quoteDisplay')
-const QuoteInput = document.getElementById('quoteInput')
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
-QuoteInput.addEventListener('input', () => {
-    console.log('changed')
+quoteInputElement.addEventListener('input', () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+  const arrayValue = quoteInputElement.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+  })
+
+  if (correct) renderNewQuote()
 })
 
-function getRquote() {
-    return fetch(RANDOM_QUOTE_URL)
-        .then(response => response.json())
-        .then(data => data.content) 
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
 }
 
-async function renderNewQ() {
-const quote =  await getRquote()
-displayquote.innerText=quote
-//console.log(quote)
-quote.split('').forEach(character => {
-    const charspan = document.createElement('span')
-    //charspan.classList.add('correct')
-    charspan.innerText=character
-    displayquote.appendChild(charspan)
-});
-QuoteInput.value=null
+async function renderNewQuote() {
+  const quote = await getRandomQuote()
+  quoteDisplayElement.innerHTML = ''
+  quote.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    quoteDisplayElement.appendChild(characterSpan)
+  })
+  quoteInputElement.value = null
+  startTimer()
 }
 
-renderNewQ()
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQuote()
